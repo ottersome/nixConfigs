e964d315474651d5787ab679939b96fd24a693f5
Luis Garcia <luisgarcia26284@gmail.com>
2024-07-31 13:12:05 +0800

    Draft

--- flake.nix
+++ flake.nix
@@ -4,9 +4,11 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.05"; # 24.05
+    # home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
@@ -15,45 +17,34 @@
     nixpkgs,
     home-manager,
     ...
-  } @ inputs: let
+  } @ inputs: 
+  let
     inherit (self) outputs;
   in {
     # NixOS configuration entrypoint
     # Available through 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations = {
-      "lab716a" = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
+      "lab716a" = nixpkgs.lib.nixosSystem rec{
+        system = "x86_64-linux";
+        pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        specialArgs = {inherit pkgs inputs;};
         # > Our main nixos configuration file <
         modules = [
             ./nixos/configuration.nix
+            # inputs.home-manager.nixosModules.default
             home-manager.nixosModules.home-manager
             {
-              # home-manager.useGlobalPkgs = true;
+              home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.racc = import ./home-manager/racc-home.nix;
+              # home-manager.extraSpecialArgs = specialArgs;
             }
           ];
       };
     };
 
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    # homeConfigurations = {
-    #   "lab716a@lab716a" = home-manager.lib.homeManagerConfiguration {
-    #     pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-    #     extraSpecialArgs = {inherit inputs outputs;};
-    #     # > Our main home-manager configuration file <
-    #     modules = [
-    #         ./home-manager/lab716a/lab716a-home.nix
-    #         ./modules/newuser.nix
-    #       ];
-    #   };
-    #   "racc@lab716a" = home-manager.lib.homeManagerConfiguration {
-    #     pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-    #     extraSpecialArgs = {inherit inputs outputs;};
-    #     # > Our main home-manager configuration file <
-    #     modules = [./home-manager/racc-home.nix];
-    #   };
-    # };
   };
 }
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -18,28 +18,27 @@
 
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
-    # home-manager.nixosModules.home-manager
   ];
 
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
-    };
-  };
+  # nixpkgs = {
+  #   # You can add overlays here
+  #   overlays = [
+  #     # If you want to use overlays exported from other flakes:
+  #     # neovim-nightly-overlay.overlays.default
+  #
+  #     # Or define it inline, for example:
+  #     # (final: prev: {
+  #     #   hi = final.hello.overrideAttrs (oldAttrs: {
+  #     #     patches = [ ./change-hello-to-hi.patch ];
+  #     #   });
+  #     # })
+  #   ];
+  #   # Configure your nixpkgs instance
+  #   config = {
+  #     # Disable if you don't want unfree packages
+  #     allowUnfree = true;
+  #   };
+  # };
 
   nix = let
     flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
@@ -60,7 +59,6 @@
     nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
 
-  # FIXME: Add the rest of your current configuration
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -141,6 +139,7 @@
       # Be sure to change it (using passwd) after rebooting!
       initialPassword = "itlablinux";
       isNormalUser = true;
+      # shell = pkgs.zsh;
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
 	"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCw0fgl5QxyFmqY1Hcg2RSCm7XLQlIQ5naFY9S1OAmOV8LnTpqjG0YLs8K8Z2bgioykH8EBj0rnyMPNj31XmXZB55ss810yOMH4M4tpKpPdyvxOIxh+IcLluj4uiq018Tzj5+zXXhDQlh4HQdxqQFCvX7n/2VPF8GpWOd74wxO0HKirBAakxH+wvVrns62XAJyKYkTSMYHg1h8UMrKf2SLO3lVdmTmLHGxpLu+sa9jJnP9bZhop049EmGye2lSvrfn+ZWKMBnbsN4byLhrXCeVIDJ/4TJuJWqZmnWWw6Nbz7y8KKm4FdkhmVY/cXiYf6+1XThWePQwxcrpwC1voosy7ZXfXldV1jwTpU5d/3t4sfJ6Wufi2gys7b9HDdAbGJU+sU4hmi99JPUp59S23/kyl7x4/utcksodXmR67OBYqUZakvT27FpI1xq6ITMH6NvyNb1zLSOlVIFlVIrZjbOkkcqg3WoAZwk6kN9mDhQrzjxvMreCesx3bXjbuI+WbnE6B/vaz62u/WYfygIRupRkiunpSRA3qzOjt+iBbGJYmXzr7v762pdleG6ALznpqE4S7SaZO7/y+KU5INBUiqeC6iYD+5HWmCM69YZnj7du+CUdyAjEFXbHFGiZ6SccNwLKPpSNfXyTHDRHnRS7A2Rcdfeo5V5eZhXXwF2L18KECKQ== luisgarcia26284@gmail.com
@@ -189,5 +188,5 @@
   #};
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  system.stateVersion = "23.05";
+  system.stateVersion = "24.05";
 }
