6e21984bbafc0c967b38d4ed3a3708e6b7e07c8d
Luis Garcia <luisgarcia26284@gmail.com>
2024-09-13 09:51:24 +0800

    Commenting: Nvidia

--- nixos/laptop_configuration.nix
+++ nixos/laptop_configuration.nix
@@ -331,15 +331,31 @@
       persistencedSha256 = "sha256-Vz33gNYapQ4++hMqH3zBB4MyjxLxwasvLzUJsCcyY4k=";
     };
 
-    prime = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
-      nvidiaBusId = "PCI:100:0:0";
-      amdgpuBusId = "PCI:101:0:0";
-    };
-  };
+  # services.xserver.videoDrivers = ["amdgpu" "nvidia"];
+  # hardware.nvidia = {
+  #   modesetting.enable = true;
+  #   powerManagement.finegrained = false;
+  #   open = true;
+  #   nvidiaSettings = true;
+  #   # package = config.boot.kernelPackages.nvidiaPackages.beta;
+  #   package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+  #     version = "550.107.02";
+  #     sha256_64bit = "sha256-+XwcpN8wYCjYjHrtYx+oBhtVxXxMI02FO1ddjM5sAWg=";
+  #     sha256_aarch64 = "sha256-mVEeFWHOFyhl3TGx1xy5EhnIS/nRMooQ3+LdyGe69TQ=";
+  #     openSha256 = "sha256-Po+pASZdBaNDeu5h8sgYgP9YyFAm9ywf/8iyyAaLm+w=";
+  #     settingsSha256 = "sha256-WFZhQZB6zL9d5MUChl2kCKQ1q9SgD0JlP4CMXEwp2jE=";
+  #     persistencedSha256 = "sha256-Vz33gNYapQ4++hMqH3zBB4MyjxLxwasvLzUJsCcyY4k=";
+  #   };
+  #
+  #   prime = {
+  #     offload = {
+  #       enable = true;
+  #       enableOffloadCmd = true;
+  #     };
+  #     nvidiaBusId = "PCI:100:0:0";
+  #     amdgpuBusId = "PCI:101:0:0";
+  #   };
+  # };
   # hardware.opengl = {
   #   enable = true;
   #   package = pkgs.mesa.drivers;
