5e4776d5e2f798d8d37c81cfdd5ddcc0cefec4f1
ottersome <luisgarcia26284@gmail.com>
2024-09-17 10:20:27 +0800

    Adding: Steam

--- nixos/laptop_configuration.nix
+++ nixos/laptop_configuration.nix
@@ -282,6 +282,14 @@
 
   programs.thunar.enable = true;
 
+  # For Steam
+  programs.steam = {
+    enable = true;
+    # remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    # dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    # localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
 
   security.polkit.enable = true;
   # Policy Kit Agent will allow us to raise privileges for certain operations.
