1ecad1ac5fcb4ab4a53878c81b34d1ae8ee4a972
ottersome <luisgarcia26284@gmail.com>
2024-10-08 08:43:37 +0800

    Removing: Nvidia from plasma. Defaults to it I have to figure how to use hybrid with it.

--- nixos/laptop_configuration.nix
+++ nixos/laptop_configuration.nix
@@ -32,6 +32,24 @@
       services.desktopManager.plasma6 = {
         enable = true;
       };
+      # TODO: Find a less harsh way of not using nvidia to drive plasma
+      # --- Start: For completely disabling nvidia ---
+      boot.extraModprobeConfig = ''
+        blacklist nouveau
+        options nouveau modeset=0
+          '';
+          services.udev.extraRules = ''
+        # Remove NVIDIA USB xHCI Host Controller devices, if present
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+        # Remove NVIDIA USB Type-C UCSI devices, if present
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+        # Remove NVIDIA Audio devices, if present
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+        # Remove NVIDIA VGA/3D controller devices
+        ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+          '';
+          boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
+
     };
     hyprland.configuration = {
       programs.waybar = {
@@ -50,23 +68,6 @@
     };
   };
 
-  # TODO: Find a less harsh way of not using nvidia to drive plasma
-  # --- Start: For completely disabling nvidia ---
-  boot.extraModprobeConfig = ''
-    blacklist nouveau
-    options nouveau modeset=0
-      '';
-      services.udev.extraRules = ''
-    # Remove NVIDIA USB xHCI Host Controller devices, if present
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
-    # Remove NVIDIA USB Type-C UCSI devices, if present
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
-    # Remove NVIDIA Audio devices, if present
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
-    # Remove NVIDIA VGA/3D controller devices
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
-      '';
-      boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
 
 
   # For pre-built CUDA packages
